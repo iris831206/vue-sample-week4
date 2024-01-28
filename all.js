411c1aaa3798397f3a83146619ea17fcf1f2a283
“iris <chidy76071919@hotmail.com>
2024-01-28 11:54:28 +0800

    git init

--- all.js
+++ all.js
@@ -5,6 +5,9 @@ const app = Vue.createApp({
       apiPath: 'iris831206',
       products: [],
       tempProduct: {},
+      isNew: false, //判斷是否為新增產品
+      modalProduct: null,
+      delProductModal: null,
     };
   },
   methods: {
@@ -21,26 +24,84 @@ const app = Vue.createApp({
         })
     },
     getData() {
+      //取得資料
       const url = `${this.apiUrl}/api/${this.apiPath}/admin/products`;
       axios.get(url)
         .then((res) => {
-          //console.log(res);
           this.products = res.data.products;
         })
         .catch((err) => {
           alert(err.response.data.message);
         })
-    }
+    },
+
+    updateProduct() {
+      //新增產品
+      let url = `${this.apiUrl}/api/${this.apiPath}/admin/product`;
+      let http = 'post';
+      //編輯產品
+      if (!this.isNew) {
+        url = `${this.apiUrl}/api/${this.apiPath}/admin/product/${this.tempProduct.id}`;
+        http = 'put'
+      }
+      axios[http](url, { data: this.tempProduct })
+        .then((res) => {
+          this.getData(); //重新渲染產品列表
+          this.modalProduct.hide(); //關掉Modal
+          this.tempProduct = {}; //清除欄位
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+        })
+    },
+    delProduct() {
+      //刪除產品
+      const url = `${this.apiUrl}/api/${this.apiPath}/admin/product/${this.tempProduct.id}`;
+      axios.delete(url, { data: this.tempProduct })
+        .then((res) => {
+          this.getData();
+          this.delProductModal.hide();
+          this.tempProduct = {};
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+        })
+    },
+    openModal(status, item) {
+      if (status === 'new') {
+        this.tempProduct = {
+          imgUrl: [],
+        };
+        this.isNew = true;
+        this.modalProduct.show();
+      } else if (status === 'edit') {
+        this.tempProduct = { ...item };
+        this.isNew = false;
+        this.modalProduct.show();
+      } else if (status === 'del') {
+        this.tempProduct = { ...item };
+        this.delProductModal.show()
+
+      }
+
+    },
+
+
   },
 
   mounted() {
-    //將token存到cookie
+    //實體化BS modal
+    this.modalProduct = new bootstrap.Modal(this.$refs.productModal);
+    this.delProductModal = new bootstrap.Modal(this.$refs.delProductModal);
+
+    //將token存到cookie以進行驗證
     const myCookie = document.cookie.replace(
       /(?:(?:^|.*;\s*)hexToken\s*\=\s*([^;]*).*$)|^.*$/,
       "$1",
     );
     axios.defaults.headers.common['Authorization'] = myCookie;
-    this.checkLogin()
+    this.checkLogin();
+
   },
 
 })
--- login.js
+++ login.js
@@ -16,23 +16,13 @@ createApp({
             const url = `${this.apiUrl}/admin/signin`;
             axios.post(url, this.user)
                 .then((res) => {
-                    console.log(res);
                     const { token, expired } = res.data;
-                    console.log(token, expired);
                     document.cookie = `hexToken=${token}; expires=${new Date(expired)}; path:/`;
                     window.location = 'index.html';
                 }).catch((err) => {
                     alert(err.response.data.message);
                 });
         },
-        
-    },
-    mounted() {
-        //將token存到cookie
-        const myCookie = document.cookie.replace(
-            /(?:(?:^|.*;\s*)hexToken\s*\=\s*([^;]*).*$)|^.*$/,
-            "$1",
-        );
-        axios.defaults.headers.common['Authorization'] = myCookie;
+
     },
 }).mount('#app');
